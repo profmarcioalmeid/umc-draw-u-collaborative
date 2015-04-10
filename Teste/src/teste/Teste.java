6ab2a1578f6294c5e447ecb730a7fb4718d3cd14
marciomendes <marciomendes@24lab115.umc.br>
2015-04-10 19:47:56 -0300

    Somente teste

--- Teste/src/teste/Teste.java
+++ Teste/src/teste/Teste.java
@@ -17,7 +17,7 @@ public class Teste {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.print("Tese git");
+        System.out.print("Tese git teste");
     }
     
 }

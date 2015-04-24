25f1f0112ed95eb2406e39f5875f96c3214d7e84
marcioalmeid <marcioalmeid@gmail.com>
2015-04-24 15:28:58 -0300

    Testando commit no novo branch

--- draw-u-collaborative/src/br/sp/umc/DrawUCollaborative.java
+++ draw-u-collaborative/src/br/sp/umc/DrawUCollaborative.java
@@ -15,7 +15,7 @@ public class DrawUCollaborative {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Testando o commit no novo branch
+        //Testando o commit no novo branch testre
     }
     
 }
